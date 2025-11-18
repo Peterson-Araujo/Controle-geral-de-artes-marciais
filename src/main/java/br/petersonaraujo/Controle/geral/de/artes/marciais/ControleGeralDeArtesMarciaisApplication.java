a1f9be92dfcb66c5f8d5133516f0ffb3dd033f1d
Peterson Araujo <petersonaraujo70@gmail.com>
2025-11-18 09:06:51 -0300

    build: add Dockerfile and docker-compose with PostgreSQL database

---
+++ src/main/java/br/petersonaraujo/Controle/geral/de/artes/marciais/ControleGeralDeArtesMarciaisApplication.java
@@ -0,0 +1,13 @@
+package br.petersonaraujo.Controle.geral.de.artes.marciais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleGeralDeArtesMarciaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleGeralDeArtesMarciaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/petersonaraujo/Controle/geral/de/artes/marciais/ControleGeralDeArtesMarciaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.petersonaraujo.Controle.geral.de.artes.marciais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGeralDeArtesMarciaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
